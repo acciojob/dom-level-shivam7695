8ee8e412aaf78877de8b375cfc6fb581c34f24d2
Shivam Jadhav <127479563+shivam7695@users.noreply.github.com>
2023-09-22 14:23:30 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 //your JS code here. If required.
+documnet.addEventListener("DOMContentLoader",() => {
+	
+
+let levelElement = document.getElementByid("level");
+	let level = 1;
+while(true){
+	if(levelElement.tagName === "HTML"){
+		break;
+	}
+	levelElement = levelElement.parentNode;
+	level++;
+}
+	alert(`The level of the element ${level}`);
+})
